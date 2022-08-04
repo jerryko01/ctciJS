e623720e0986de1caa41a82127935b119f3fc2a7
jerryko01 <85685925+jerryko01@users.noreply.github.com>
2022-08-04 23:08:25 +0900

    Create linkedLists.js

---
+++ Chapter_Two/linkedLists.js
@@ -0,0 +1,130 @@
+const n1 = {
+    data: 100
+}
+
+const n2 = {
+    data: 200
+}
+
+n1.next = n2;
+
+// n1 = {data: 100, next: {data: 200}}
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // Insert first node
+    insertFirst(data) {
+        this.head = new Node(data, this.head)
+        this.size += 1;
+    }
+
+    // Insert last node
+    insertLast(data) {
+        let node = new Node(data);
+        let current;
+
+        // If empty, make head
+        if (!this.head) {
+            this.head = node;
+        } else {
+            current = this.head;
+            while (current.next) {
+                current = current.next
+            }
+            current.next = node;
+        }
+        this.size += 1;
+    }
+
+    // Insert at index
+    insertAt(data, index) {
+        // If index is out of range
+        if (index > 0 && index > this.size) {
+            return;
+        }
+        // If first index
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        const node = new Node(data);
+        let current, previous;
+
+        // Set current to first
+        current = this.head;
+        let count = 0;
+
+        while (count < index) {
+            previous = current; // Node before index
+            count += 1;
+            current = current.next; // Node after index
+        }
+
+        node.next = current;
+        previous.next = node;
+
+        this.size += 1;
+    }
+
+    // Get at index
+    getAt(index) {
+        let current = this.head;
+        let count = 0;
+        while (current) {
+            if (count === index) {
+                console.log(current.data)
+            }
+            count += 1;
+            current = current.next
+        }
+        return null;
+    }
+
+    // Remove at index
+    removeAt(index) {
+        if (index > 0 && index > this.size) {
+            return;
+        }
+        let current = this.head;
+        let previous;
+        let count = 0;
+        if (index === 0) {
+            this.head = current.next;
+        }
+        else {
+            while (count < index) {
+                count += 1;
+                previous = current;
+                current = current.next;
+            }
+            previous.next = current.next;
+        }
+    }
+
+    // Clear list
+    clearList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    //Print list data
+    printListData() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
